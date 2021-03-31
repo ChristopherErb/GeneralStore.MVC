f7b3213879173adf7fc5fb46c37f514d329790d8
ChristopherErb01 <christophererb01@gmail.com>
2021-03-31 19:19:32 -0400

    First Commit

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,139 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+   
+    public class ProductController : Controller
+    {
+        //Add the application DB Context (link to the database)
+        // GET: Product
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        private ApplicationDbContext _db = new ApplicationDbContext();
+           
+
+
+
+        public ActionResult Index()
+        {
+            List<Product> productList = _db.Products.ToList();
+            List<Product> orderedList = productList.OrderBy(prod => prod.Name).ToList();
+            return View(orderedList);
+
+
+        //   return View(_db.Products.ToList());
+
+        }
+
+        //Post Prudct
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //Prroduct/Delete/{ID}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = _db.Products.Find(id);
+            if (product == null)                    
+                {
+                    return HttpNotFound();
+                   
+                }
+            return View(product);
+                }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //GET Edit
+        // Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+
+        //POST : Edit /Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+
+        //Get:Details
+        //Product/Details/{id}
+
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            Product product = _db.Products.Find(id);
+
+            if (product==null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+
+
+
+
+}
